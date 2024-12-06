22e650cbbd522d30ef254a28ff7acff33a48e8d3
joshuayeyo <chiikawayeyo@gmail.com>
2024-12-07 01:09:03 +0900

    Refactor: Modify App.tsx 파일 import 구문 절대경로로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import '@/styles/style.css'
+import App from '@/App'
 createRoot(document.getElementById('root')!).render(
     <App />
 )
