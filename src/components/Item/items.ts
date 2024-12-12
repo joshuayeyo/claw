e4b04be4054a7d11b0a0a96a5ce3c4bd2831139e
joshuayeyo <chiikawayeyo@gmail.com>
2024-12-12 10:39:23 +0900

    Feat(#5): Add 뽑기를 위한 상품 목록

---
+++ src/components/Item/items.ts
@@ -0,0 +1,17 @@
+import { ItemList } from "@/types" 
+
+export const prizes: ItemList = [
+    { id: 1, name:'꽝', image: 'bomb.png'},
+    { id: 2, name:'100 포인트', image: '100Point.png'},
+    { id: 3, name:'1000 포인트', image: '1000Point.png'},
+    { id: 4, name:'토끼 인형', image: 'rabitDoll.png'},
+    { id: 5, name:'강아지 인형', image: 'dogDoll.png'},
+    { id: 6, name:'쿠로미 인형', image: 'kuromiDoll.png'},
+    { id: 7, name:'한교동 인형', image: 'hangyodong.png'},
+    { id: 8, name:'롯데타워', image: 'lotteTower.png'},
+    { id: 9, name:'개복치', image: 'gaebok.png'},
+    { id: 10, name:'아이폰', image: 'iphone.png'},
+    { id: 11, name:'포르쉐', image: 'porsche.png'},
+    { id: 12, name:'삼성전자 1주', image: 'samsung.png'},
+    { id: 13, name:'10000 포인트', image: '10000Point.png'},
+]
