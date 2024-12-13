12e0798bdf1513ae78e2e785368f35e5c16e5cae
joshuayeyo <chiikawayeyo@gmail.com>
2024-12-14 01:24:38 +0900

    Feat(#10): Add 음성인식, Claw 기능 Board에 연결

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,11 +1,45 @@
 import styled from "@emotion/styled"
 import { prizes } from "@/components/Item/items";
-import Item from "../Item";
+import Item from "@/components/Item";
 import { useEffect, useState } from "react";
+import Claw from "@/components/Claw";
+import GameStartModal from "../modals/GameStart";
+import { useSpeechControl } from "@/hooks/useSpeechControl";
+import useClawControl from "@/hooks/useClawControl";
 
 const Board = () => {
     // 정렬을 위해 아이템 랜덤하게 섞어야 합니다.
     const [shuffledPrizes, setShuffledPrizes] = useState<typeof prizes>([]);
+    const [gameStarted, setGameStarted] = useState(false);
+
+    const { position, startMoving, stopMoving } = useClawControl();
+
+    // 게임 시작 상태 체크
+    const startGame = () => {
+        if (!gameStarted) {
+            setGameStarted(true);
+        }
+    }
+
+    const pickItem = () => {
+        console.log("아이템을 뽑습니다.")
+        // 세부 로직 구현 예정
+    }
+
+    // 음성인식 시작
+    const { startListening } = useSpeechControl({
+        onStart: startGame,
+        onUp: () => startMoving("up"),
+        onDown: () => startMoving("down"),
+        onLeft: () => startMoving("left"),
+        onRight: () => startMoving("right"),
+        onStop: stopMoving,
+        onPick: pickItem,
+    });
+
+    useEffect(() => {
+        startListening();
+    }, [startListening])
 
     // 13개의 아이템을 중복 허용하여 16개 가져오기
     useEffect(() => {
@@ -26,6 +60,8 @@ const Board = () => {
 
     return(
         <Wrapper>
+            {/* 게임이 아직 시작되지 않았다면, 게임 시작 창을 띄웁니다. */}
+            {!gameStarted && <GameStartModal onStart={startGame} />}
             <PlayGround>
                 <ItemsContainer>
                     {/* 뽑아야 할 아이템들 정렬; 16개의 칸으로 구성 */}
@@ -33,10 +69,7 @@ const Board = () => {
                         <Item id={item.id} name={item.name} image={item.image} />
                     ))}
                 </ItemsContainer>
-                <ClawContainer>
-                    {/* 초기에 봉을 위치시킬 영역 */}
-                </ClawContainer>
-
+                { gameStarted && <Claw position={position}/> }
             </PlayGround>
         </Wrapper>
     )
@@ -63,6 +96,8 @@ const PlayGround = styled.div`
     background-color: #c0c0c0;
     width: 80%;
     padding: 20px;
+    overflow: hidden;
+    position: relative; // claw 위치 제어
 `
 
 const ItemsContainer = styled.div`
@@ -70,10 +105,5 @@ const ItemsContainer = styled.div`
     height: 90%;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-template-columns: repeat(4, 1fr);
     gap: 10px;
-`
-
-const ClawContainer = styled.div`
-
 `
