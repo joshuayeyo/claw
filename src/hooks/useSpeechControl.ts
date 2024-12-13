e00e5dfde1be6b761cb6204b4aca24b141864e17
joshuayeyo <chiikawayeyo@gmail.com>
2024-12-14 01:24:17 +0900

    Feat(#10): Add "뽑기" 명령어 추가

--- src/hooks/useSpeechControl.ts
+++ src/hooks/useSpeechControl.ts
@@ -8,9 +8,10 @@ type Props = {
     onLeft: () => void;
     onRight: () => void;
     onStop: () => void;
+    onPick: () => void;
 }
 
-export const useSpeechControl = ({ onStart, onUp, onDown, onLeft, onRight, onStop }: Props) => {
+export const useSpeechControl = ({ onStart, onUp, onDown, onLeft, onRight, onStop, onPick }: Props) => {
     const { transcript, resetTranscript } = useSpeechRecognition();
 
     const startListening = () => {
@@ -23,8 +24,6 @@ export const useSpeechControl = ({ onStart, onUp, onDown, onLeft, onRight, onSto
 
     // 실시간으로 음성 명령 처리
     useEffect(() => {
-        console.log(transcript)
-
         if (transcript.includes("게임 시작")) {
             onStart();
             resetTranscript(); // 명령 실행 후 transcript를 초기화합니다.
@@ -43,8 +42,10 @@ export const useSpeechControl = ({ onStart, onUp, onDown, onLeft, onRight, onSto
         } else if (transcript.includes("멈춰")) {
             onStop();
             resetTranscript();
-        } else {
-            console.log("올바른 명령어를 말하세요");
+        } else if (transcript.includes("뽑기")) {
+            onPick();
+            resetTranscript();
+        }   else {
             return;
         }
     }, [transcript, onUp, onDown, onLeft, onRight]);
