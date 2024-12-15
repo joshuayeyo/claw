fd228d0a4e5fb9ff970f20c8136b6a10fcfe1857
joshuayeyo <chiikawayeyo@gmail.com>
2024-12-15 16:38:04 +0900

    Refactor(#8): Modify claw speed 상수 새로 선언

--- src/hooks/useClawControl.ts
+++ src/hooks/useClawControl.ts
@@ -1,14 +1,20 @@
 import { useRef, useState } from "react";
 
-type Direction = "up" | "down" | "left" | "right";
+type Direction = "up" | "down" | "left" | "right" | "reset";
 
 const useClawControl = () => {
     const [position, setPosition] = useState({ top: 90, left: 3 });
     const moveInterval = useRef<NodeJS.Timeout | null>(null);
 
     const startMoving = (direction: Direction) => {
-        stopMoving();
+        const clawXSpeed = 1;
+        const clawYSpeed = 1;
 
+        stopMoving();   // restart 시 claw 위치 고정
+        if (direction === "reset") {
+            setPosition({ top: 90, left: 3});
+            return;
+        }
         moveInterval.current = setInterval(() => {
             setPosition((prev) => {
                 let newTop = prev.top;
@@ -16,16 +22,16 @@ const useClawControl = () => {
 
                 switch (direction) {
                     case "up":
-                        newTop = Math.max(prev.top - 2, 0); // 위로 이동
+                        newTop = Math.max(prev.top - clawYSpeed, 0); // 위로 이동
                         break;
                     case "down":
-                        newTop = Math.min(prev.top + 2, 100); // 아래로 이동
+                        newTop = Math.min(prev.top + clawYSpeed, 100); // 아래로 이동
                         break;
                     case "left":
-                        newLeft = Math.max(prev.left - 0.8, 0); // 왼쪽으로 이동
+                        newLeft = Math.max(prev.left - clawXSpeed, 0); // 왼쪽으로 이동
                         break;
                     case "right":
-                        newLeft = Math.min(prev.left + 0.8, 100); // 오른쪽으로 이동
+                        newLeft = Math.min(prev.left + clawXSpeed, 100); // 오른쪽으로 이동
                         break;
                 }
 
